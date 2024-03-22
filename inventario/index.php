4d1933b7a734a38d22113ba1e22207caeb266131
Ángela Amanda Sánchez Barbosa <61953402+angelexi76@users.noreply.github.com>
2024-03-22 18:21:13 -0500

    first commit

---
+++ inventario/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<?php include "./inc/head.php"; ?>
+</head>
+<body>
+<?php include "./inc/menu-burger.php"; ?>
+ <?php include "./inc/navbar.php"; ?>
+</body>
+</html>
